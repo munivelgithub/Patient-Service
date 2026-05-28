316904923990b0c972afcaf642d54b8ef1bdc7f1
munivel.javadev@gmail.com <munivel.javadev@gmail.com>
2026-05-28 11:36:09 +0530

    Project start

---
+++ src/test/java/com/muni/Patient/Service/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muni.Patient.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
