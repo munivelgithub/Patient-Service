2044161b0dfd36ff1828168b070e02f5abfe185e
Munivel <munivel.javadev@gmail.com>
2026-05-28 17:04:34 +0530

    Created H2 Db connection

---
+++ src/main/java/com/muni/Patient/Service/model/Patient.java
@@ -0,0 +1,41 @@
+package com.muni.Patient.Service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "Patient")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @NotNull
+    private String name;
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+    @NotNull
+    @Column(columnDefinition = "Text")
+    private String address;
+
+    @NotNull
+    private LocalDate dateofBirth;
+
+    @Column(unique = true)
+    @NotNull
+    private long phonenumber;
+
+    @NotNull
+    private LocalDate registeredDate;
+}
